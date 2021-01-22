936dac7aab75b2e8c06b0888b710f6219b4df3ca
rudolfssau <rudolfssau@gmail.com>
2021-01-22 23:27:17 +0200

    Fixes bugs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,24 +20,24 @@
             }
         </style>
     </head>
-    <body id="body-main">
-        <header>
-                @if (Route::has('login'))
-                                <div id="body-nav" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                                    @auth
-                                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                                    @else
-                                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-                                        @if (Route::has('register'))
-                                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                                        @endif
-                                    @endauth
-                                </div>
-                            @endif
-            </header>
+    <body>
         <div id="body-content" class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
-            <h1 id="body-content-header">Online Assignments</h1>
+            @if (Route::has('login'))
+                <div id="body-header" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <div id="body-content">
+                        <h1>Online Assignemts</h1>
+                    </div>
         </div>
     </body>
 </html>
