85a5aab0ec6be221d06c2dbc6842e4ec55fd73fc
rudolfssau <rudolfssau@gmail.com>
2021-01-29 23:09:06 +0200

    Adds fixes with styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,16 +26,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <div id="janis">Online Assignments</div>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
