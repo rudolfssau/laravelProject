152c278268251004c3e2e2d16feea5e57d962c83
rudolfssau <rudolfssau@gmail.com>
2021-02-02 18:28:52 +0200

    Adds todays view

--- resources/views/content/content.blade.php
+++ resources/views/content/content.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-    Index
+    <div id="content-main">
+        <div id="todaysview-container">
+            <h1 id="todaysview">Todays view</h1>
+        </div>
+    </div>
 @endsection
