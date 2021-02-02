71dc960fdff06c734b874b1bb41346f33f666423
rudolfssau <rudolfssau@gmail.com>
2021-02-02 17:49:59 +0200

    reworking the backend

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,31 @@
   <meta charset="utf-8">
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <title>Online Assigments</title>
 </head>
 <body>
+    <header>
+        <nav>
+            <ul id="main-nav-ul">
+                <li>
+                    <a href="home">Home</a>
+                </li>
+                <li>
+                    <a href="classroom">Classroom</a>
+                </li>
+                <li>
+                    <a href="assignments">Assignments</a>
+                </li>
+                <li>
+                    <a href="mail">OS-Mail</a>
+                </li>
+                <li>
+                    <a href="lessons">Lessons</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
   @yield('content')
 </body>
 </html>
