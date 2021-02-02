be22f8af987865e59a605541f17cf2e6b664cad0
rudolfssau <rudolfssau@gmail.com>
2021-02-02 14:33:58 +0200

    reworking the backend

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/content', function () {
+    return view('content.content');
 });
 
 Auth::routes();
